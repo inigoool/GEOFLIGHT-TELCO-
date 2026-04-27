c24413b76fbe448654fab2b6a8951fa6b1af7567
inigoool <ijb336@alumnos.unican.es>
2026-04-27 13:30:34 +0200

    Create main.c

---
+++ main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    printf("GEOFLIGHT TELCO - Sistema de control de vuelo iniciado.\n");
+    printf("Validacion de Integracion Continua finalizada con exito.\n");
+    return 0;
+}
